33b917e517c034fb61217c255ef2303e9e3f480e
ray <rillep90@gmail.com>
2023-04-11 23:42:42 +0200

    Adding install script

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-set -oeu pipefail
+set -e
+
+# Check if the script is run with sudo
+if [ "$(id -u)" -ne 0 ]; then
+    echo "This script must be run with sudo. Try: sudo $0" >&2
+    exit 1
+fi
 
 # Update repos
 sudo apt update
@@ -8,19 +14,22 @@ sudo apt update
 # Upgrade system
 sudo apt upgrade -y
 
-required_packages="i3 i3lock i3blocks rofi feh picom arandr scrot xfce4-terminal thunar pavucontrol neovim"
+# Packages required for setting up environment
+required_packages="i3 rofi nitrogen picom arandr scrot thunar pavucontrol neovim alsa-utils \
+                arandr betterlockscreen brightnessctl lightdm lxappearance neofetch \
+                network-manager network-manager-gnome numlockx pavucontrol polybar pulseaudio \
+                python3-i3ipc qt5ct ranger suckless-tools xfce4-power-manager xorg slick-greeter kitty"
 
+# Additional packages
 additional_packages="timeshift neofetch nala ripgrep btop htop adb fastboot tor torbrowser-launcher wireguard ncdu bat"
 
+# Cybersecurity related packages
 cybersec_packages="hcxdumptool hcxtools realtek-rtl88xxau-dkms wifiphisher"
 
 # Install packages
-sudo apt install -y \
-    required_packages \
-    additional_packages \
-    cybersec_packages 
+sudo apt install -y $required_packages $additional_packages $cybersec_packages
 
-
+# Updating tldr database
 tldr -u
 
 # Vscode
@@ -36,23 +45,25 @@ wget "${DOWNLOAD_URL}" -O /tmp/obsidian.deb
 sudo apt install /tmp/obsidian.deb
 rm /tmp/obsidian.deb
 
-# Nvim 
+# Nvim
 wget -O /tmp/nvim-linux64.deb https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.deb
 sudo apt install /tmp/nvim-linux64.deb
 rm /tmp/nvim-linux64.deb
 git clone https://github.com/nvim-lua/kickstart.nvim.git ~/.config/nvim
 
+# Create dirs
+mkdir -p -v ~/Development ~/Misc
 
-mkdir -p ~/Development ~/Misc
+cp -R wallpapers ~/Pictures
+sudo cp wallpapers/login.jpg /usr/share/backgrounds/xfce/
+rm -R wallpapers
 
+sudo cp -R lightdm /etc/lightdm
+rm -R lightdm
 
-
-
-#cp -R .config ~/                                            
-#chmod -R +x ~/.config/i3/scripts
-#cd ..
-#rm -rf endeavouros-i3wm-setup
+cp -R * ~/.config
+rm ~/.config/README.md ~/.config/install.sh
 
 # Clean up
 sudo apt autoremove -y
-sudo apt autocleann
+sudo apt autoclean
